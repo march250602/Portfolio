ca25be77a6910b7dde514f7f22a3613ad56af2c4
Ratchanon Pawanja <68798300+march250602@users.noreply.github.com>
2024-03-08 18:07:00 -0500

    Update index.js

--- src/components/Project1/index.js
+++ src/components/Project1/index.js
@@ -46,7 +46,40 @@ const Porject1 = () => {
               The dataset is large and complex, making it challenging to analyze effectively.<br/>
           To address  this, I need to create a data model and extract the essential information.
               <img  src={dataset} alt="JavaScript Developer Name, Web Developer Name" style={{ width: '800px'}} />
-              <br/><br/><img  src={Model} alt="JavaScript Developer Name, Web Developer Name" style={{ width: '800px'}} />
+              <br/><br/> 
+                 <div className="tex-describe" style={{ left: '10%',top: '50%',width: '70%',maxheight: '90%'}}>
+              <h3> Fact Table </h3> 
+<p >This table serves as the central repository for all relevant information about taxi trips.
+It includes foreign keys that link to the various dimension tables to provide additional context and details about each trip.<br/>
+</p>
+<h3> Datetime_Dim Table</h3>
+
+Stores date and time-related information for each taxi trip, such as pick-up and drop-off times.<br/>
+It includes attributes like date, day, month, year, hour and weekday.<br/>
+<h3>Pickup_Location_Dim Table</h3>
+
+Contains details about the pick-up locations, such as zones and location IDs.<br/>
+<h3>Dropoff_Location_Dim Table</h3>
+
+Similar to the pickup location dimension but for drop-off locations.<br/>
+<h3>Passenger_Count_Dim Table</h3>
+
+Stores information about the number of passengers for each trip.<br/>
+It includes attributes like passenger count ranges or special passenger types.<br/>
+<h3>Rate_Code_Dim Table</h3>
+
+Stores details about the rate codes applied to each taxi trip.<br/>
+
+<h3>Payment_Type_Dim Table</h3>
+
+Contains information about the payment types used for taxi trips.<br/>
+This include cash, credit card, vouchers, etc.<br/>
+<h3>Trip_Distance_Dime Table</h3>
+
+Stores data related to the distance traveled for each trip.<br/>
+</div>
+
+                  <br/><img  src={Model} alt="JavaScript Developer Name, Web Developer Name" style={{ width: '800px'}} />
               
     
             </p>
